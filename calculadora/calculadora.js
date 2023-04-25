c5dc76dda2562fd29361bdbc9d7d82822ca2882c
Davi de Almeida Guimarães <odaviloper@gmail.com>
2023-04-25 17:35:51 -0300

    0 funcionando, e outras funções também

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -1,18 +1,22 @@
 class Buttons {
-    
     constructor () {
         document.querySelectorAll('.button').forEach(element => {
             element.addEventListener('click', () => {
                 this._getFunctionOf(element.id)
             })
             this[`_${element.id}`] = element
-            console.log(element.id)
+        });
+        this[`_number-0`] = document.querySelector('#number-0')
+        this[`_number-0`].addEventListener('click', () => {
+            this._getFunctionOf('number-0')
         });
     }
+
     _getFunctionOf(elementId) {
         if (this.isElementNumber(elementId)) return this.addNumberToVisor(elementId)
+        
         const functionsByElements = {
-            'allclear': '',
+            'allclear': this.allClear(),
             'plusminus': '',
             'percentage': '',
             'divide': '',
@@ -21,17 +25,34 @@ class Buttons {
             'sum': '',
             'equals': '',
         }
+        functionsByElements[elementId]
     }
     addNumberToVisor(number) {
+        this.removeAllClear()
         return addVisor(this.getValueFrom(number))
     }
+
     getValueFrom(elementId) {
         return this[`_${elementId}`].textContent
     }
+
     isElementNumber(elementId) {
         return elementId.startsWith('number-')
     }
+
+    allClear() {
+        this.setAllClear()
+        return setVisor(0);
+    }
+    removeAllClear() {
+        document.querySelector('#allclear').innerHTML = 'C'
+    }
+    setAllClear() {
+        document.querySelector('#allclear').innerHTML = 'AC'
+    }
+    
 }
+
 function getVisor() {
     return parseInt(document.querySelector('.visor').innerHTML)
 }
@@ -41,8 +62,6 @@ function addVisor(value) {
 function setVisor(value = 0) {
     return document.querySelector('.visor').innerHTML = value 
 }
-function allClear() {
-    return setVisor(0);
-}
-const buttons = new Buttons()
-console.log(buttons.getValueFrom('number-7'))
+
+const buttons = new Buttons();
+
